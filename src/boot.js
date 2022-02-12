f354d9bcc68d404b337118f903f7f586ee146528
miggon23 <miggon23@ucm.es>
2022-02-12 01:14:05 +0100

    Limpieza de plantilla

--- src/boot.js
+++ src/boot.js
@@ -1,10 +1,4 @@
-/**
- * Escena para la precarga de los assets que se usarán en el juego.
- * Esta escena se puede mejorar añadiendo una imagen del juego y una 
- * barra de progreso de carga de los assets
- * @see {@link https://gamedevacademy.org/creating-a-preloading-screen-in-phaser-3/} como ejemplo
- * sobre cómo hacer una barra de progreso.
- */
+
 export default class Boot extends Phaser.Scene {
   /**
    * Constructor de la escena
@@ -17,12 +11,7 @@ export default class Boot extends Phaser.Scene {
    * Carga de los assets del juego
    */
   preload() {
-    // Con setPath podemos establecer el prefijo que se añadirá a todos los load que aparecen a continuación
-    this.load.setPath('assets/sprites/');
-    this.load.image('platform', 'platform.png');
-    this.load.image('base', 'base.png');
-    this.load.image('star', 'star.png');
-    this.load.image('player', 'player.png');
+    
   }
 
   /**
--- src/end.js
+++ src/end.js
@@ -1,8 +1,4 @@
-/**
- * Escena de fin de juego. Cuando se han recogido todas las estrellas, se presenta un
- * texto que indica que el juego se ha acabado.
- * Si se pulsa cualquier tecla, se vuelve a iniciar el juego.
- */
+
 export default class End extends Phaser.Scene {
   /**
    * Constructor de la escena
@@ -19,10 +15,6 @@ export default class End extends Phaser.Scene {
         .setOrigin(0.5, 0.5)  // Colocamos el pivote en el centro de cuadro de texto 
         .setAlign('center');  // Centramos el texto dentro del cuadro de texto
 
-    // Añadimos el listener para cuando se haya pulsado una tecla. Es probable que no
-    // lleguemos a ver el mensaje porque veníamos con una tecla pulsada del juego (al 
-    // ir moviendo al jugador). Se puede mejorar añadiendo un temporizador que 
-    // añada este listener pasado un segundo
     this.input.keyboard.on('keydown', function (event) { 
       this.scene.start('level');
     }, this);
