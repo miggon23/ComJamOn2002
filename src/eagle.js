a72a5d6c8b4c122e7dae446d24ebcbe65e277b0c
miggon23 <miggon23@ucm.es>
2022-02-12 13:22:08 +0100

    Collider ajustado del aguila

--- src/eagle.js
+++ src/eagle.js
@@ -3,13 +3,15 @@ export default class Eagle extends Phaser.GameObjects.Sprite
     constructor(scene, x, y){
         super(scene, x, y, 'eagle')
 
+        this.setScale(0.1);
         this.scene.add.existing(this);
         this.scene.physics.add.existing(this, true);
         this.setDepth(2);
 
-        this.setScale(0.1);
 
-        this.body.setSize(this.displayWidth * 0.3, this.displayHeight * 0.35, true);
+        this.body.setSize(this.displayWidth * 0.7, this.displayHeight * 0.15, true);
+        this.body.setOffset(this.displayWidth * 0.15, this.displayHeight * 0.35);
+        
     }
 
 }
